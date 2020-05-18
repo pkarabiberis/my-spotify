8d16c6c419615fb97bc5d03e63582c1a4987a25e
pkarabiberis <61578516+pkarabiberis@users.noreply.github.com>
2020-05-18 17:26:04 +0300

    add top artists&tracks

---
+++ client/src/components/Nav.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="nav">
+    <h4>Profile</h4>
+    <h4>Top artists</h4>
+    <h4>Top tracks</h4>
+    <h4>Recent</h4>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.nav {
+  background-color: rgb(4, 3, 6);
+  color: white;
+  flex-direction: column;
+  min-height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100px;
+  text-align: center;
+  z-index: 99;
+}
+
+h4 {
+  padding: 5px;
+}
+</style>
